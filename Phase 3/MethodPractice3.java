a09a196c1c2b19d31448803fb55005c917adaf58
Cesar <214414970+medina-cesar@users.noreply.github.com.>
2025-12-11 10:35:04 -0500

    added method practice 3

---
+++ Phase 3/MethodPractice3.java
@@ -0,0 +1,28 @@
+public class MethodPractice3 {   
+    public static void main (String[] args){
+    int[] nums = {432, 2000, 543, 2};
+
+    int max = max_finder(nums);
+
+    System.out.println("Max number" +max);
+    
+}
+
+public static int max_finder(int[] nums) {
+    int i;
+    int max = 0;
+    int current_num = 0;
+
+    for(i = 0; i < nums[i]; i++){
+        if(current_num < nums[i]){
+            current_num = max;
+        }
+
+    }
+
+    return max;
+}
+
+
+
+}
