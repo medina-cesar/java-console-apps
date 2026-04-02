61b7cc579d22d2d60f645cfb44afb4ef773a7ccd
Cesar <214414970+medina-cesar@users.noreply.github.com>
2026-04-02 13:11:35 -0400

    Delete Phase 1/HelloWorld.java

--- Phase 1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Cesar!");
-        System.out.print("yooo, Cesar!");
-        System.out.printf("Hello %s, you are %d years old.", "Cesar", 27);
-    }
-}
